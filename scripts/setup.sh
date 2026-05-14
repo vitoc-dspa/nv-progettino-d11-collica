d2b77fd28c28f1375b4b633e33ee57f48dd9407f
Vito Collica <vito.collica@students.uniroma2.eu>
2026-05-14 11:26:26 +0200

    Fix setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 sudo docker-compose pull
 # Crea certificato SSL self-signed in certs_mount (key.key + cert.crt)
-mkdir -p certs_mount
-openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+sudo mkdir -p certs_mount
+sudo openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
   -keyout certs_mount/key.key \
   -out certs_mount/cert.crt \
   -subj "/CN=localhost"
 
-chmod 600 certs_mount/key.key
-chmod 644 certs_mount/cert.crt
+sudo chmod 600 certs_mount/key.key
+sudo chmod 644 certs_mount/cert.crt
 sudo docker-compose up -d
 sudo docker compose exec ollama ollama pull qwen3.5:2b
